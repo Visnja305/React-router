5755b107aaf8873155d829280d2a2503f51c470f
Visnja305 <105449754+Visnja305@users.noreply.github.com>
2023-10-17 16:00:30 -0400

    final

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,22 @@
-function Movies() {
+import { NavLink, Switch, Route } from "react-router-dom";
+import MovieDetails from "../MovieDetails";
+
+function Movies({ movies }) {
   return (
     <div className="comp orange">
       <h1>Movies Component</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+          </li>
+        ))}
+        <Switch>
+          <Route path="/movies/:movieId">
+            <MovieDetails movies={movies} />
+          </Route>
+        </Switch>
+      </ul>
     </div>
   );
 }
